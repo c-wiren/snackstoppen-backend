68253f84f18e148d771de20a178d09f702be63f0
Christian Wirén <christian@wiren.cc>
2021-04-07 21:18:21 +0300

    dockerfile deploy

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-GOARCH=amd64 GOOS=linux go build -o build/server *.go
+GOARCH=amd64 GOOS=linux go build -ldflags="-w -s" -o build/server *.go
 git subtree push --prefix build dokku main
